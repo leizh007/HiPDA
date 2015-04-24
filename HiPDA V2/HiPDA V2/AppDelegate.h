d1cdfeec869f9541d7d50835935bc033bd878693
leizh007 <leizh007@qq.com>
2015-04-24 23:03:02 +0800

    v2

---
+++ HiPDA V2/HiPDA V2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HiPDA V2
+//
+//  Created by leizh007 on 15/4/24.
+//  Copyright (c) 2015年 leizh007. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HiPDA V2/HiPDA V2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HiPDA V2
+//
+//  Created by leizh007 on 15/4/24.
+//  Copyright (c) 2015年 leizh007. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
