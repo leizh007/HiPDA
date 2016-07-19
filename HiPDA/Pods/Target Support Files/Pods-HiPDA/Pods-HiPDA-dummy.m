4cf8cc6e6986eddf8c6405588441086367908c91
leizh007 <leizh007@qq.com>
2016-07-19 21:22:11 +0800

    update .gitignore

--- HiPDA/Pods/Target Support Files/Pods-HiPDA/Pods-HiPDA-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HiPDA : NSObject
-@end
-@implementation PodsDummy_Pods_HiPDA
-@end
--- HiPDA/Pods/Target Support Files/Pods-HiPDAUITests/Pods-HiPDAUITests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_HiPDAUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_HiPDAUITestsVersionString[];
-
