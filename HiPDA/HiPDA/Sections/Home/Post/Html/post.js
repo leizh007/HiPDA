8a2f86f9957b69e9b20209142db8c1c6897e750c
leizh007 <leizh007@qq.com>
2017-05-24 20:11:24 +0800

    change font color if font color is white

--- HiPDA/HiPDA/Sections/Home/Post/Html/post.js
+++ HiPDA/HiPDA/Sections/Home/Post/Html/post.js
@@ -22,6 +22,9 @@ function adjustFontSize() {
         } else {
             font.style.fontSize = "17px";
         }
+        if (hexToRgb(font.getAttribute("color")) === "rgb(255, 255, 255)") {
+            font.setAttribute("color", font.parentElement.tagName === "A" ? "#0000ee" : "#000000");
+        }
     }
 }
 
@@ -180,4 +183,24 @@ function clientJSCodeReady() {
     var readyEvent = doc.createEvent("Events");
     readyEvent.initEvent("clientJSApiOnReady");
     doc.dispatchEvent(readyEvent);
+}
+              
+// https://stackoverflow.com/questions/9421208/how-to-compare-a-backgroundcolor-in-javascript
+// rgb中间有空格，如下形式: "rgb(255, 255, 255)"
+function hexToRgb(hex) {
+    if (hex == null || hex == undefined) {
+        return null;
+    }
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? "rgb(" + [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+    ].join(', ') + ")" : null;
 }
