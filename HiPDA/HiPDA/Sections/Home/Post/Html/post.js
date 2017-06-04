dc00f43be9a73de67420e660ae251bab6aeb5412
leizh007 <leizh007@qq.com>
2017-06-04 11:39:26 +0800

    scroll smooth js

--- HiPDA/HiPDA/Sections/Home/Post/Html/post.js
+++ HiPDA/HiPDA/Sections/Home/Post/Html/post.js
@@ -198,11 +198,12 @@ setupWebViewJavascriptBridge(function (bridge) {
     bridge.registerHandler("jumpToPid", function (data, responseCallback) {
         var pid = data;
         var posts = document.getElementsByClassName("post");
-        window.location.hash = "#post_" + pid;
+        // window.location.hash = "#post_" + pid;
         for (var i = 0; i < posts.length; ++i) {
             posts[i].setAttribute("style", "background-color: #ffffff !important;");
         }
         document.getElementById("post_" + pid).setAttribute("style", "background-color: #eeeeee !important;");
+        smoothScroll("post_" + pid);
     });
 })
 
@@ -403,3 +404,53 @@ function imagesToShowInPost(post) {
 function imgLoaded(imgElement) {
     return imgElement.complete && imgElement.naturalHeight !== 0;
 }
+
+// https://stackoverflow.com/questions/10063380/javascript-smooth-scroll-without-the-use-of-jquery
+
+function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
+
+
+function elmYPosition(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    } return y;
+}
+
+
+function smoothScroll(eID) {
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+        scrollTo(0, stopY); return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+}
+
