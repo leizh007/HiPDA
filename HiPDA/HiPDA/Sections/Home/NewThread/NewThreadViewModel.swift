c7f6106f6180d45ac7cbed8583f3f30d468ee75e
leizh007 <leizh007@qq.com>
2017-06-14 00:55:46 +0800

    new thread

--- HiPDA/HiPDA/Sections/Home/NewThread/NewThreadViewModel.swift
+++ HiPDA/HiPDA/Sections/Home/NewThread/NewThreadViewModel.swift
@@ -27,7 +27,8 @@ typealias NewTheadResult = HiPDA.Result<Int, NewThreadError>
 class NewThreadViewModel {
     fileprivate var disposeBag = DisposeBag()
     func postNewThread(fid: Int, typeid: Int, title: String, content: String, completion: @escaping (NewTheadResult) -> Void = { _ in }) {
-        NetworkUtilities.formhash(from: "/forum/post.php?action=newthread&fid=\(fid)") { result in
+        NetworkUtilities.formhash(from: "/forum/post.php?action=newthread&fid=\(fid)") { [weak self] result in
+            guard let `self` = self else { return }
             switch result {
             case .success(let formhash):
                 HiPDAProvider.request(.newThread(fid: fid, typeid: typeid, title: title, content: content, formhash: formhash))
@@ -37,16 +38,7 @@ class NewThreadViewModel {
                     .subscribe { event in
                         switch event {
                         case .next(let html):
-                            do {
-                                let tid = try HtmlParser.tid(from: html)
-                                completion(.success(tid))
-                            } catch {
-                                if let errorMessage = try? HtmlParser.newThreadErrorMessage(from: html) {
-                                    completion(.failure(.unKnown(errorMessage)))
-                                } else {
-                                    completion(.failure(.unKnown(error.localizedDescription)))
-                                }
-                            }
+                            self.handleNewThreadResult(html, completion: completion)
                         case .error(let error):
                             completion(.failure(.unKnown(error.localizedDescription)))
                         default:
@@ -54,6 +46,19 @@ class NewThreadViewModel {
                         }
                     }.disposed(by: self.disposeBag)
             case .failure(let error):
+                completion(.failure(.unKnown(error.localizedDescription)))
+            }
+        }
+    }
+    
+    fileprivate func handleNewThreadResult(_ result: String, completion: @escaping (NewTheadResult) -> Void = { _ in }) {
+        do {
+            let tid = try HtmlParser.tid(from: result)
+            completion(.success(tid))
+        } catch {
+            if let errorMessage = try? HtmlParser.newThreadErrorMessage(from: result) {
+                completion(.failure(.unKnown(errorMessage)))
+            } else {
                 completion(.failure(.unKnown(error.localizedDescription)))
             }
         }
