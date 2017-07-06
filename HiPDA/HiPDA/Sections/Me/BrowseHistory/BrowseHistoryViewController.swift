da4caa08b1ec554b3ab604ed33fe914d46db5d89
leizh007 <leizh007@qq.com>
2017-07-06 23:14:25 +0800

    browse history

---
+++ HiPDA/HiPDA/Sections/Me/BrowseHistory/BrowseHistoryViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BrowseHistoryViewController.swift
+//  HiPDA
+//
+//  Created by leizh007 on 2017/7/6.
+//  Copyright © 2017年 HiPDA. All rights reserved.
+//
+
+import Foundation
---
+++ HiPDA/HiPDA/Sections/Me/BrowseHistory/BrowseHistoryViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  BrowseHistoryViewModel.swift
+//  HiPDA
+//
+//  Created by leizh007 on 2017/7/6.
+//  Copyright © 2017年 HiPDA. All rights reserved.
+//
+
+import Foundation
